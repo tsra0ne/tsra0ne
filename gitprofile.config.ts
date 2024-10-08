7f928e496903df8aa6f673ceadbd32f70f0e6b4d
Sravan Goud <tsravangoud164@gmail.com>
2024-10-08 18:33:41 +0530

    changed project url

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/theswiftguy'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['tsra0ne/theswiftguy'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
